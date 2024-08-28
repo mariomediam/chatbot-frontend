65aaf06e9d0a20560ab2c8313c19b99296247174
Your Name <you@example.com>
2024-08-28 12:22:59 -0500

    chore: add toolbar Search TUPA and bakground search

--- frontend/src/views/AdminProcedure.jsx
+++ frontend/src/views/AdminProcedure.jsx
@@ -3,11 +3,13 @@ import { useState, useRef } from "react";
 import { Header } from "../components/Header";
 import { Breadcrumb } from "../components/Breadcrumb";
 import ListIcon from "../components/icons/ListIcon";
+import imgBackgroundSearch from "../assets/background-search.svg";
 
 export const AdminProcedure = () => {
   const [isSaving, setIsSaving] = useState(false);
 
   const inputSearch = useRef(null);
+  //   const selectSearch = useRef(null);
 
   const itemsBreadCrumb = [{ name: "Administrar TUPA", url: "/admin-tupa" }];
 
@@ -27,11 +29,12 @@ export const AdminProcedure = () => {
   ];
 
   const [categorySearchSelected, setCategorySearchSelected] = useState(
-    categorySearch[0]
+    categorySearch[0].id
   );
 
-  const selectCategorySearchChange = (category) => {
-    setCategorySearchSelected(category);
+  const selectCategorySearchChange = (e) => {
+    console.log("Se seleccionó la categoría", e.target.value);
+    setCategorySearchSelected(e.target.value);
     inputSearch.current.focus();
   };
 
@@ -55,90 +58,32 @@ export const AdminProcedure = () => {
           un nuevo procedimiento debe ir a la opción Importar TUPA.
         </p>
 
-        <form className="max-w-lg mx-auto">
+        <form className="max-w-2xl w-full  mt-5 mx-auto">
           <div className="flex">
-            {/* <label
-              for="search-dropdown"
-              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >
-              Your Email
-            </label>
-            <button
-              id="dropdown-button"
-              data-dropdown-toggle="dropdown"
-              className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
-              type="button"
-            >
-              {categorySearchSelected.name}
-              <svg
-                className="w-2.5 h-2.5 ms-2.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </button>
-            <div
-              id="dropdown"
-              className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-            >
-              <ul
-                className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdown-button"
-              >                
-                {categorySearch
-                  .filter(
-                    (category) => category.id !== categorySearchSelected.id
-                  )
-                  .map((category) => (
-                    <li>
-                      <button
-                        type="button"
-                        className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                        onClick={() => selectCategorySearchChange(category)}
-                      >
-                        {category.name}
-                      </button>
-                    </li>
-                  ))}
-              </ul>
-            </div> */}
             <select
-              id="years"
-            //   size="5"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 max-w-[140px]"
+              id="category-search"
+              //   size="5"
+              className="bg-bg_primary-100 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 max-w-[140px]"
+              onChange={selectCategorySearchChange}
             >
-
-                 {categorySearch.map((category) => (
-                    <option
-                        onClick={() => selectCategorySearchChange(category)}
-                      >
-                        {category.name}
-                      </option>
-                    
-                  ))}
-              
+              {categorySearch.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
             </select>
             <div className="relative w-full">
               <input
                 type="search"
                 id="search-dropdown"
-                className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-                placeholder="Search Mockups, Logos, Design Templates..."
+                className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-bg_primary-100 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+                // placeholder="Search Mockups, Logos, Design Templates..."
                 ref={inputSearch}
                 required
               />
               <button
                 type="submit"
-                className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
               >
                 <svg
                   className="w-4 h-4"
@@ -149,9 +94,9 @@ export const AdminProcedure = () => {
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                   />
                 </svg>
@@ -161,6 +106,23 @@ export const AdminProcedure = () => {
           </div>
         </form>
       </div>
+
+      <div className="flex justify-center mx-4">
+        <div className="flex flex-col w-full max-w-2xl justify-center items-center border border-gray bg-bg_primary-100 rounded-lg p-3">
+          <p className="m-0 p-0 text-sm">
+            Por favor, ingrese sus criterios de búsqueda para comenzar
+          </p>
+          <div className="w-full flex justify-center">
+            <img
+              src={imgBackgroundSearch}
+              alt="Imagen buscar"
+              className="max-w-xs pt-0 mt-0 relative -top-[20px]"
+            />
+          </div>
+        </div>
+      </div>
+      
+
       {/* <Toaster richColors visibleToasts={9} position="top-right" />
       {isSaving && <Spinner />} */}
     </div>
